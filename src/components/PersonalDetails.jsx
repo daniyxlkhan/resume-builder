826f50cc5c56f862c701cd37ea4483ffd853bb16
Javantax <79591148+Javantax@users.noreply.github.com>
2025-07-17 08:18:13 -0300

    Add visual adjustments for education & personal details

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -3,47 +3,45 @@ import InputGroup from './InputGroup';
 
 function PersonalDetails({ onChange, fullName, email, phoneNumber, location}) {
     return (
-        <>
-            <div className="personal-details">
-                <h1>Personal Details</h1>
-                <InputGroup
-                    type="text"
-                    id="full-name"
-                    labelText="Full name"
-                    placeHolder="First and last name"
-                    value={fullName}
-                    onChange={onChange}
-                    dataKey="fullName"
-                />
-                <InputGroup
-                    type="email"
-                    id="email"
-                    labelText="Email Address"
-                    placeHolder="Email address"
-                    value={email}
-                    onChange={onChange}
-                    dataKey="email"
-                />
-                <InputGroup
-                    type="tel"
-                    id="phone-number"
-                    labelText="Phone number"
-                    placeHolder="Phone number"
-                    value={phoneNumber}
-                    onChange={onChange}
-                    dataKey="phoneNumber"
-                />
-                <InputGroup
-                    type="text"
-                    id="location"
-                    labelText="Location"
-                    placeHolder="Location"
-                    value={location}
-                    onChange={onChange}
-                    dataKey="location"
-                />
-            </div>
-        </>
+        <div className="personal-details">
+            <h1>Personal Details</h1>
+            <InputGroup
+                type="text"
+                id="full-name"
+                labelText="Full name"
+                placeHolder="First and last name"
+                value={fullName}
+                onChange={onChange}
+                dataKey="fullName"
+            />
+            <InputGroup
+                type="email"
+                id="email"
+                labelText="Email Address"
+                placeHolder="Email address"
+                value={email}
+                onChange={onChange}
+                dataKey="email"
+            />
+            <InputGroup
+                type="tel"
+                id="phone-number"
+                labelText="Phone number"
+                placeHolder="Phone number"
+                value={phoneNumber}
+                onChange={onChange}
+                dataKey="phoneNumber"
+            />
+            <InputGroup
+                type="text"
+                id="location"
+                labelText="Location"
+                placeHolder="Location"
+                value={location}
+                onChange={onChange}
+                dataKey="location"
+            />
+        </div>
     );
 }
 
